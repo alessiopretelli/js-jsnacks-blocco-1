d9e7e0fd6b6d124430e9a762273d6cc83e4e2671
Alessio <ale79631@gmail.com>
2021-03-12 14:42:19 +0100

    fine

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 //es.1
 
 var pesoTotale = 0;
@@ -52,7 +53,7 @@ var zucchine = [
         'peso': 19,
         'lunghezza': 96
     },
-]
+];
 
 for (i = 0; i < zucchine.length; i++) {
     pesoTotale += zucchine[i]['peso'];
@@ -94,13 +95,13 @@ console.log('Peso totale > 15: ' + pTmg15);
 //es.3 
 
 function reverse(p) {
-var reversed = [];
+var reversed = "";
 
-        for (i = p.length - 1; i >= 0; i--) {
-            reversed[p.length - (i + 1)] = p[i];
-        }
+    for (i = p.length - 1; i >= 0; i--) {
+        reversed += p[i];
+    }
 
-return reversed.join('');    
+return reversed;    
 }
     
 var parola;
@@ -110,3 +111,111 @@ parola = prompt("Inserisci una parola.");
 } while (!isNaN(parola));
 
 console.log(reverse(parola));
+*/
+
+
+//******ESERCIZI ASSEGNATI 12/3/21******
+
+//1)
+/*
+var nomi = ['Homer', 'Lisa', 'Marge', 'Maggie', 'Bart'];
+var cognomi = ['Simpson', 'Burns', 'Szislak', 'Johnson', 'Bouvier'];
+var nomeI = '';
+var cognomeI = '';
+var invitati = [];
+var v = 0;
+
+for (i = 0; i < 3; i++) {   
+nomeI = nomi[Math.floor(Math.random() * nomi.length)];
+cognomeI = cognomi[Math.floor(Math.random() * cognomi.length)];
+invitati[i] = nomeI + ' ' + cognomeI;
+
+    for (ii = 0; ii < i; ii++) {
+        
+        if (invitati[i] == invitati[ii]) {
+            i--;
+        }
+
+    }
+
+}
+
+for (i = 0; i < 3; i++) {
+    console.log('Invitato: ' + invitati[i]);
+}
+*/
+
+//2)
+/*
+var num = [3, 5, 7, 22, 46, 96, 101, 8];
+var somma = 0;
+
+for (i = 0; i < num.length; i++) {
+    
+    if (i % 2 != 0) {
+        console.log(num[i]);
+        somma += num[i];
+    }
+
+}
+
+console.log(num);
+console.log('Somma dei numeri in posizione "informatica" dispari: ' + somma);
+*/
+
+//3)
+/*
+var numOne = [7, 5, 99, 4, 6, 78, 88, 989, 332];
+var numTwo = [111, 222, 333, 444];
+
+console.warn("Lunghezza dei due array prima dell'aggiunta di elementi casuali:");
+console.log(numOne.length);
+console.log(numTwo.length);
+
+
+for (i = numTwo.length; i < numOne.length; i++) {
+    numTwo[i] = Math.floor((Math.random() * 1000) + 1);
+}
+
+console.warn("Lunghezza dei due array dopo l'aggiunta di elementi casuali:");
+console.log(numOne.length);
+console.log(numTwo.length);
+*/
+
+//4)
+/*
+function merge(l, n) {
+    var merged = [];
+
+    for (i = 0; i < l.length; i++) {
+        merged.push(l[i]);
+        merged.push(n[i]);
+    }
+
+    return merged;
+}
+
+var lettere = ['a', 'b', 'c'];
+var numeri = [1, 2, 3];
+
+console.log(merge(lettere, numeri));
+*/
+
+//5)
+/*
+function between(n, aa, bb) {
+    var nn = [];
+    //compresi tra a e b esclusi, posizione 'informatica'
+    for (i = aa + 1; i < bb; i++) {
+        nn.push(n[i]);
+    }
+
+    return nn;
+}
+
+var num = [0, 1, 2, 3, 4, 5, 6, 7];
+var a = 3;
+var b = 6;
+
+console.log('Array di elementi in posizione "informatica" tra 3(a) e 6(b) esclusi: ' + between(num, a , b));
+*/
